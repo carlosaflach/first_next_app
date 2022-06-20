fca37ef26cbec6e48a8feed4e8874c5be323e9eb
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-19 22:43:44 -0300

    Implementing server side rendering.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
-import styles from '../styles/home.module.scss';
+import { GetServerSideProps } from "next";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+interface Post {
+  id: string;
+  title: string;
+}
+
+export default function Home({ posts }: HomeProps) {
+  // const[posts, setPosts] = useState<Post[]>([]);
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/posts').then((response) => response.json().then((data) => setPosts(data)));
+  // }, [])
+  
   return (
-    <h1>Hello World <span>With Next.js</span></h1>
+    <div>
+    <h1>Posts</h1>
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+    </div>
   )
+}
+
+interface HomeProps {
+  posts: Post[];
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> =  async (context) => {
+  const response = await fetch('http://localhost:3333/posts');
+  const posts = await response.json();
+  return {
+    props: {
+      posts,
+    },
+  };
 }
