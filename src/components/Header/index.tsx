1cabeb0197f03ce65b204a351b3b883ef22dc8a8
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-19 21:47:22 -0300

    Header component pt2.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,23 @@
 import styles from './styles.module.scss';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export const Header = () => {
+
+  const router = useRouter();
+  const { asPath } = router;
+
   return (
     <header className={styles.container}>
       <div className={styles.content}>
         <img src="logo.svg" alt="Dev News!" />
         <nav>
-          <a href="#">Home</a>
-          <a href="#">Posts</a>
+          <Link href="/">
+            <a className={asPath === '/' ? styles.active : ''}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>
+          </Link>
         </nav>
       </div>
     </header>
